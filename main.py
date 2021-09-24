de89e81af977461e78202308c20e242102e81ce9
Aarush <amazingaarush999@gmail.com>
2021-09-24 19:15:20 +0800

    deleted imgcap and camera

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import numpy
+from cv2 import cv2
 from djitellopy import tello
-import time
-
 
 drone = tello.Tello()
 drone.connect()
-print(drone.get_battery())
-drone.takeoff()
-time.sleep(10)
-drone.land()
+drone.streamon()
+
+while True:
+    img = drone.get_frame_read().frame
+    cv2.imshow("feed", img)
+    if cv2.waitKey(1) & 0xff == ord("q"):
+        break
+
+
+
+cv2.destroyWindow()
+
 
 
 
