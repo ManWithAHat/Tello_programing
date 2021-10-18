62b339682ca69b41a8170771bfda68f56e2014e5
Aarush <amazingaarush999@gmail.com>
2021-10-18 21:07:32 +0800

    yup im done

--- functions.py
+++ functions.py
@@ -5,14 +5,25 @@ from djitellopy import Tello
 def inittello():
     drone = Tello()
     drone.connect()
-    drone.set_speed_x(0)
-    drone.set_speed_y(0)
-    drone.set_speed_z(0)
     drone.streamon()
+
     return drone
 
-def telloframe(drone,w = 360,h = 240):
+
+
+def telloframe(drone, w=360, h=240):
     frame = drone.get_frame_read().frame
-    img = cv2.cv2.resize(frame,(w,h))
+    img = cv2.cv2.resize(frame, (w, h))
     return img
 
+
+def adjust(drone, vector):
+    fb = 0
+    if vector[2] >= 10:
+        fb = -20
+    elif vector[2] <= -10:
+        fb = 20
+    elif vector[2] > -10 or vector[2] < 10:
+        fb = 0
+
+    drone.send_rc_control(0,fb,-(int(vector[0])/10),0)
--- main.py
+++ main.py
@@ -1,6 +1,41 @@
+import numpy
 from cv2 import cv2
 from cv2 import data
+from djitellopy import tello
+
+import functions
 from functions import *
 
-drone = inittello()
+
+drone = functions.inittello()
+drone.takeoff()
+
+face_cascade = cv2.CascadeClassifier(data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+PID = [0.5, 0.5, 0]
+
+while True:
+    img = telloframe(drone)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    dif = [0, 0, 0]
+    wxis = 0
+    for (x, y, w, h) in faces:
+        if w > wxis:
+            wxis = w
+        cenx = x + (w / 2)
+        ceny = y + (h / 2)
+        cv2.circle(img, (int(cenx), int(ceny)), 5, (255, 255, 0), 5)
+        dif = [cenx - 320, ceny - 240, 175 - w]
+    cv2.circle(img, (180, 120), 5, (255, 255, 0), 5)
+    print(dif)
+    functions.adjust(drone,dif)
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) & 0xff == ord('k'):
+        break
+
+cv2.destroyAllWindows()
+drone.land()
 
